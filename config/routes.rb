eccd20f4f42d9302e26f44c707f5acaa4db45ee5
Aestimo K. <aestimo.njuguna@gmail.com>
2025-05-01 12:41:48 +0300

    Added advanced search routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+ # Advanced search (POST request)
+ get "advanced_search", to: "home#advanced_search", as: :advanced_search
+ post "search_results", to: "home#search_results", as: :search_results
+
   get "up" => "rails/health#show", as: :rails_health_check
   root "home#index"
 end
